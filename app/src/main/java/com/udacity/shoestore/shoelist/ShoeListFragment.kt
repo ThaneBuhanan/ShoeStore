69696f26219a2137484932dfe2968bda45cb4c4c
ThaneBuhanan <thanebuhanan@gmail.com>
2023-02-24 21:05:28 -0700

    packages created for fragments.

--- app/src/main/java/com/udacity/shoestore/shoelist/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/shoelist/ShoeListFragment.kt
@@ -1,10 +1,11 @@
-package com.udacity.shoestore
+package com.udacity.shoestore.shoelist
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.udacity.shoestore.R
 
 class ShoeListFragment : Fragment() {
 
