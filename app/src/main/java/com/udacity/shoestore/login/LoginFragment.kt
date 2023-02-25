bd28a8bf3313b1f8e4a03bb292cbff579495d78f
ThaneBuhanan <thanebuhanan@gmail.com>
2023-02-24 21:11:44 -0700

    login implemented.

--- app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
@@ -23,6 +24,6 @@ class LoginFragment: Fragment() {
     }
 
     private fun launchWelcomeScreen() {
-        TODO("Not yet implemented")
+        findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
     }
 }
