8a50181ab6be88d6be4879d6b4017fd55c45b595
ThaneBuhanan <thanebuhanan@gmail.com>
2023-02-24 21:49:01 -0700

    buttons navigate, shoe list views changed.

--- app/src/main/java/com/udacity/shoestore/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/welcome/WelcomeFragment.kt
@@ -1,32 +1,32 @@
 package com.udacity.shoestore.welcome
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = WelcomeFragment()
-    }
-
-    private lateinit var viewModel: WelcomeViewModel
+    private val viewModel: WelcomeViewModel = ViewModelProvider(this)[WelcomeViewModel::class.java]
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        val binding = DataBindingUtil.inflate<FragmentWelcomeBinding>(
+            inflater, R.layout.fragment_welcome, container, false
+        )
+        binding.seeInstructions.setOnClickListener { launchInstructions() }
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(WelcomeViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun launchInstructions() {
+        findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToInstructionsFragment())
     }
-
 }
